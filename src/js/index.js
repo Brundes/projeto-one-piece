710bcf8e4778934a0ea050ef888742b335b32721
Brundes <pedro.pac97@gmail.com>
2023-10-02 08:54:50 -0300

    index.html - feat - Added id on the buttons estilo - fix - adjusted color background index.js - feat - created the JS code

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+
+
+const botoes = document.querySelectorAll(".botao");
+
+botoes.forEach(botao => {
+    botao.addEventListener("click", () => {
+        const botaoSelecionado = document.querySelector(".botao.selecionado");
+        botaoSelecionado.classList.remove("selecionado")
+
+
+        botao.classList.add("selecionado");
+    });
+});
